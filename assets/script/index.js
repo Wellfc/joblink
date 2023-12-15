73b5352d9365332452e88929c8200fc36ffa2473
Wellington Suero <144365343+Wellfc@users.noreply.github.com>
2023-12-15 14:00:35 -0600

    Add login validation and show invalid message

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-import { onEvent, select, print } from './utils.js';
+import { onEvent, select, sleep, print } from './utils.js';
 
 const usernameInput = select('.input-username');
 const passwordInput = select('.input-password');
@@ -11,4 +11,27 @@ const invalidLogin = select('.invalid-login');
 localStorage.setItem('username', 'jobseeker');
 localStorage.setItem('password', 'hired1234');
 
+print(localStorage.getItem('username'));
+print(localStorage.getItem('password'));
+
 // Validate the user's login info
+function validateLogin() {
+    const username = usernameInput.value.trim();
+    const password = passwordInput.value.trim();
+
+    print(username);
+    print(password);
+
+    if (username === localStorage.getItem('username') &&
+        password === localStorage.getItem('password')) {
+        window.location.href = 'home.html';
+    } else {
+        invalidLogin.classList.remove('hidden');
+        sleep(3000).then(() => {
+            invalidLogin.classList.add('hidden');
+        });
+    }
+};
+
+// Login button event listener
+onEvent(loginButton, 'click', validateLogin);
